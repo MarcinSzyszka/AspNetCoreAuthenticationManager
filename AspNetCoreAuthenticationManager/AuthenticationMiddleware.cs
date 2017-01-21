281b68b3e3396a637540595db5e037f77efd1b73
Ćwiara <szyszka_marcin@wp.pl>
2017-01-21 14:08:37 +0100

    Initial commit

---
+++ AspNetCoreAuthenticationManager/AuthenticationMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCoreAuthenticationManager
+{
+	public static class AuthenticationMiddleware
+	{
+		public static IApplicationBuilder UseAuthentication(this IApplicationBuilder app, string securityKey, string issuerName, string audienceName)
+		{
+			return app.Use(next => async ctx =>
+			{
+				var tokenManager = TokenManager.GetInstance(securityKey, issuerName, audienceName);
+				try
+				{
+					var authCookie = ctx.Request.Cookies.FirstOrDefault(c => c.Key == Consts.AUTHENTICATION_COOKIE_NAME);
+					if (!String.IsNullOrEmpty(authCookie.Value))
+					{
+						var claims = tokenManager.ValidateToken(authCookie.Value);
+
+						ctx.User = claims;
+					}
+					await next(ctx);
+				}
+				catch (Exception exc)
+				{
+					if (ctx.Response.HasStarted)
+					{
+						throw exc;
+					}
+
+					ctx.Response.Cookies.Append(Consts.AUTHENTICATION_COOKIE_NAME, "deleted", new CookieOptions { Expires = DateTime.Now.AddDays(-1) });
+					ctx.User = new ClaimsPrincipal();
+				}
+			});
+		}
+	}
+}
---
+++ AspNetCoreAuthenticationManager/TokenManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AspNetCoreAuthenticationManager
+{
+	public class TokenManager
+	{
+		static TokenManager tokenManagerInstance;
+		SigningCredentials signingCredentials;
+		TokenValidationParameters tokenValidationParapeters;
+		JwtSecurityTokenHandler handler;
+
+		internal TokenManager(string securityKey, string issuerName, string audienceName)
+		{
+			var securityKeyBytes = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.Default.GetBytes(securityKey));
+			signingCredentials = new SigningCredentials(securityKeyBytes, SecurityAlgorithms.HmacSha256Signature);
+			tokenValidationParapeters = new TokenValidationParameters
+			{
+				IssuerSigningKey = signingCredentials.Key,
+				ValidateLifetime = true,
+				ValidAudience = audienceName,
+				ValidIssuer = audienceName
+			};
+
+			handler = new JwtSecurityTokenHandler();
+		}
+
+		internal ClaimsPrincipal ValidateToken(string token)
+		{
+			var jwtToken = handler.ReadToken(token);
+			var claims = handler.ValidateToken(token, tokenValidationParapeters, out jwtToken);
+
+			return claims;
+		}
+
+		internal static TokenManager GetInstance(string securityKey, string issuerName, string audienceName)
+		{
+			if (tokenManagerInstance == null)
+			{
+				tokenManagerInstance = new TokenManager(securityKey, issuerName, audienceName);
+			}
+
+			return tokenManagerInstance;
+		}
+
+		public static TokenManager GetInstance()
+		{
+			return tokenManagerInstance;
+		}
+
+		public string GetTokenString(ClaimsIdentity claims, TimeSpan tokenValidTime)
+		{
+			var tokenDescriptor = new SecurityTokenDescriptor();
+			tokenDescriptor.Issuer = tokenValidationParapeters.ValidIssuer;
+			tokenDescriptor.Audience = tokenValidationParapeters.ValidAudience;
+			tokenDescriptor.SigningCredentials = signingCredentials;
+			tokenDescriptor.Subject = claims;
+			tokenDescriptor.Expires = DateTime.Now.Add(tokenValidTime);
+
+			var cookieToken = handler.CreateToken(tokenDescriptor);
+
+			return handler.WriteToken(cookieToken);
+		}
+
+		public void AppendTokenCookieToResponse(ClaimsIdentity claims, TimeSpan tokenValidTime, HttpResponse response)
+		{
+			var token = GetTokenString(claims, tokenValidTime);
+
+			response.Cookies.Append(Consts.AUTHENTICATION_COOKIE_NAME, token, new CookieOptions { Expires = DateTime.Now.Add(tokenValidTime) });
+		}
+	}
+}
